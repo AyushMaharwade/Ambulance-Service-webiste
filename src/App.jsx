629edf82d3d84af9e6ca78af654587e17a6262a0
AyushMaharwade <ayushmaharwade58@gmail.com>
2024-09-24 09:48:46 +0530

    new file:   .gitignore 	new file:   README.md 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/assets/images/Ambulance-amico (1).png 	new file:   public/assets/images/Ambulance-amico.png 	new file:   public/assets/images/Ambulance-bro (1).png 	new file:   public/assets/images/Ambulance-bro.png 	new file:   public/assets/images/Ambulance-pana (1).png 	new file:   public/assets/images/Ambulance-pana (2).png 	new file:   public/assets/images/Ambulance-pana (3).png 	new file:   public/assets/images/Ambulance-pana.png 	new file:   public/assets/images/Ambulance-rafiki.png 	new file:   src/App.jsx 	new file:   src/components/Navbar.jsx 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   tailwind.config.js 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+
+const App = () => {
+
+  return (
+    <>
+    <Navbar />
+    <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello world!</h1>
+    </>
+  )
+}
+
+export default App
